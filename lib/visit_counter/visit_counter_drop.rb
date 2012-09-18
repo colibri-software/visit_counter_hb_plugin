7e634998fb50bc0d8573d47f9871cb14338be736
Alex Sanford <alex.sanford1@gmail.com>
2012-09-18 12:59:19 -0300

    Removed mobile_detection code

--- lib/visit_counter/visit_counter_drop.rb
+++ lib/visit_counter/visit_counter_drop.rb
@@ -1,67 +1,7 @@
 
 
 
-module MobileDetection
-  class MobileDetectionDrop < ::Liquid::Drop
-
-    # TODO: make this forbidden
-    def initialize(source)
-      @source = source
-    end
-
-    def config_value
-      @source.config['my_plugin_config']
-    end
-
-    def is_mobile_device?
-      if self.source
-        self.source.is_mobile_device?
-      else
-        false
-      end
-    end
-
-    def mobile_type
-      types_to_look_for = ["android", "ipad", "iphone", "blackberry", "windows phone", "iemobile", "webos", "nokia", "kindle"]
-      types_to_look_for.each do |type|
-        if is_device?(type)
-          return type
-        end
-      end
-      return "none"
-    end
-
-    def is_android?
-      is_device?('android')
-    end
-
-    def is_iphone?
-      is_device?('iphone')
-    end
-
-    def is_blackberry?
-      is_device?('blackberry')
-    end
-
-    def is_ipad?
-      is_device?('ipad')
-    end
-
-    def is_ios?
-      is_device?('ipad')  || is_device?('iphone')
-    end
-
-    protected
-
-    attr_reader :source
-
-    def is_device?(deviceString)
-      if self.source
-        self.source.is_device?(deviceString)
-      else
-        false
-      end
-    end
-
+module VisitCounter
+  class VisitCounter < ::Liquid::Drop
   end
 end
